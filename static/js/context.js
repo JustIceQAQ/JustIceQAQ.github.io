aa00bd689265ce535c2764118bd5304ef71347a8
justiceqaq <justiceqaq@gmail.com>
2024-02-12 18:57:40 +0800

    fix: icon and project images

--- static/js/context.js
+++ static/js/context.js
@@ -30,11 +30,11 @@ const SKILL = {
     ],
     "Framework": [
         new Block("Django", "https://www.djangoproject.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg"),
-        new Block("Django Rest Framework", "https://www.django-rest-framework.org", imagesFromGoogleDrive("1NUZB4bUhcGMWbCyWIdqJM7UuSsDSnUWE")),
+        new Block("Django Rest Framework", "https://www.django-rest-framework.org", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/djangorest/djangorest-original.svg"),
         new Block("Flask", "https://flask.palletsprojects.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg"),
         new Block("FastAPI", "https://fastapi.tiangolo.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-plain-wordmark.svg"),
-        new Block("Tornado", "https://www.tornadoweb.org", imagesFromGoogleDrive("1-OU-ourDguRNj5jSOgoHXe2K26uVkImU")),
-        new Block("Sanic", "https://sanicframework.org/", "https://i.imgur.com/mp5jLhD.webp"),
+        new Block("Tornado", "https://www.tornadoweb.org", "https://img93.pixhost.to/images/853/445466390_0cga4cn.png"),
+        new Block("Sanic", "https://sanic.dev/en/", "https://i.imgur.com/mp5jLhD.webp"),
 
 
         new Block("dotnet", "https://zh.wikipedia.org/wiki/.NET", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-original-wordmark.svg"),
@@ -43,47 +43,55 @@ const SKILL = {
 
     ],
     "Frontend_Development": [
+        new Block("Streamlit", "https://streamlit.io/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/streamlit/streamlit-original-wordmark.svg"),
+
         new Block("Bootstrap", "https://getbootstrap.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"),
         new Block("Materializecss", "https://materializecss.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"),
         new Block("html5", "https://whatwg.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"),
         new Block("css3", "https://www.w3.org/Style/CSS/Overview.en.html", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"),
         new Block("jQuery", "https://jquery.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg"),
-        new Block("jQueryui", "https://jqueryui.com/", imagesFromGoogleDrive("1AJTMx1g2RVMjC4qamlwvb4Jl7My3kUNW")),
-        new Block("jquerymobile", "https://jquerymobile.com/", imagesFromGoogleDrive("1hcSBBuBv6cqrB24cXGSlzYWsGgMFlQOG")),
+        new Block("jQueryui", "https://jqueryui.com/", "https://img93.pixhost.to/images/853/445466432_rdfxgne.png"),
+        new Block("jquerymobile", "https://jquerymobile.com/", "https://img93.pixhost.to/images/853/445466385_000.png"),
 
     ],
     "Backend_Development": [
         new Block("Anaconda", "https://www.anaconda.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/anaconda/anaconda-original-wordmark.svg"),
         new Block("GraphQL", "https://graphql.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg"),
-        new Block("RabbitMQ", "https://www.rabbitmq.com/", "https://www.vectorlogo.zone/logos/rabbitmq/rabbitmq-icon.svg"),
+        new Block("RabbitMQ", "https://www.rabbitmq.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rabbitmq/rabbitmq-original-wordmark.svg"),
         new Block("Nginx", "https://nginx.org/en", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg")
     ],
     "AI_ML": [
         new Block("NumPy", "https://numpy.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg"),
         new Block("Pandas", "https://pandas.pydata.org", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original-wordmark.svg"),
-        new Block("Seaborn", "https://seaborn.pydata.org/", "https://seaborn.pydata.org/_images/logo-mark-lightbg.svg"),
         new Block("OpenCV", "https://opencv.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opencv/opencv-original-wordmark.svg"),
-        new Block("scikit-learn", "https://scikit-learn.org/", "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg"),
+        new Block("scikit-learn", "https://scikit-learn.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg"),
     ],
-    "Database": [
+    "Database_Cache": [
         new Block("Sqlalchemy", "https://www.sqlalchemy.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original-wordmark.svg"),
         new Block("postgresql", "https://www.postgresql.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"),
-        new Block("MSSQL", "https://www.microsoft.com/zh-tw/sql-server", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain-wordmark.svg"),
+        new Block("MSSQL", "https://www.microsoft.com/zh-tw/sql-server", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original-wordmark.svg"),
         // new Block("MySQL", "https://www.mysql.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"),
         // new Block("MariaDB", "https://mariadb.org/", "https://www.vectorlogo.zone/logos/mariadb/mariadb-ar21.svg"),
         new Block("MongoDB", "https://www.mongodb.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"),
         new Block("SQLite", "https://www.sqlite.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg"),
         new Block("Redis", "https://redis.io/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original-wordmark.svg"),
+        new Block("Memcached", "https://memcached.org/", "https://img93.pixhost.to/images/854/445474762_memcached-tutorial.png"),
+
+
         new Block("MinIO", "https://min.io/", "https://www.vectorlogo.zone/logos/minioio/minioio-ar21.svg")
     ],
     "DataVisualization": [
+        new Block("matplotlib", "https://matplotlib.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original-wordmark.svg"),
+        new Block("Seaborn", "https://seaborn.pydata.org/", "https://seaborn.pydata.org/_images/logo-mark-lightbg.svg"),
+        new Block("Plotly", "https://plotly.com/python/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/plotly/plotly-original-wordmark.svg"),
+
         new Block("ChartJs", "https://www.chartjs.org", "https://www.chartjs.org/media/logo-title.svg"),
     ],
     "Devops": [
         new Block("Docker", "https://www.docker.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg"),
-        new Block("Podman", "", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/podman/podman-original.svg"),
+        new Block("Podman", "https://podman.io/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/podman/podman-original.svg"),
         new Block("Kubernetes", "https://kubernetes.io/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain-wordmark.svg"),
-        new Block("Rancher", "https://rancher.com/", "https://www.vectorlogo.zone/logos/rancher/rancher-ar21.svg"),
+        new Block("Rancher", "https://rancher.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rancher/rancher-original-wordmark.svg"),
         new Block("GCP", "", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg"),
         new Block("bash", "", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"),
         new Block("Jenkins", "https://www.jenkins.io/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg")
@@ -97,13 +105,13 @@ const SKILL = {
         new Block("Selenium", "https://www.selenium.dev/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg")
     ],
     "Software": [
-        new Block("Slack", "https://slack.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg"),
+        new Block("Slack", "https://slack.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/slack/slack-original-wordmark.svg"),
         new Block("Figma", "https://www.figma.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"),
-        new Block("Postman", "https://www.postman.com/", "https://www.vectorlogo.zone/logos/getpostman/getpostman-ar21.svg"),
+        new Block("Postman", "https://www.postman.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original-wordmark.svg"),
         new Block("Filezilla", "https://filezilla-project.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/filezilla/filezilla-plain.svg"),
         new Block("Trello", "https://trello.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain-wordmark.svg"),
         new Block("Sourcetree", "https://www.sourcetreeapp.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sourcetree/sourcetree-original-wordmark.svg"),
-        new Block("Sentry", "https://sentry.io/welcome/", imagesFromGoogleDrive("1UMVcKqtjVE8ZGK9KqUyQVWVnUJEnVcRJ"))
+        new Block("Sentry", "https://sentry.io/welcome/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sentry/sentry-original-wordmark.svg")
     ],
 
     "Hardware": [
@@ -112,7 +120,7 @@ const SKILL = {
 
     "OS": [
         new Block("Windows", "https://zh.wikipedia.org/zh-tw/Microsoft_Windows", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg"),
-        new Block("Ubuntu", "https://zh.wikipedia.org/zh-tw/Ubuntu", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain-wordmark.svg"),
+        new Block("Ubuntu", "https://zh.wikipedia.org/zh-tw/Ubuntu", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ubuntu/ubuntu-original-wordmark.svg"),
         new Block("macOS", "https://zh.wikipedia.org/zh-tw/MacOS", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apple/apple-original.svg"),
     ],
     "Automation": [
@@ -123,9 +131,9 @@ const SKILL = {
         new Block("requests", "https://requests.readthedocs.io", "https://i.imgur.com/KASUQRV.webp"),
         new Block("httpx", "https://www.python-httpx.org/", "https://raw.githubusercontent.com/encode/httpx/master/docs/img/logo.jpg"),
         new Block("aiohttp", "https://docs.aiohttp.org/en/stable/index.html", "https://docs.aiohttp.org/en/stable/_static/aiohttp-plain.svg"),
-        new Block("BeautifulSoup", "https://www.crummy.com/software/BeautifulSoup/", imagesFromGoogleDrive("18L0diSP6a_prRoGjQaGjJAwkDk7LVQl0")),
-        new Block("Scrapy", "https://scrapy.org", imagesFromGoogleDrive("1_jNHjx4T6Z2ZLUEqynWR-FsuUKwYTa_B")),
-        new Block("playwright-python", "https://github.com/microsoft/playwright-python", "https://playwright.dev/python/img/playwright-logo.svg"),
+        new Block("BeautifulSoup", "https://www.crummy.com/software/BeautifulSoup/", "https://img93.pixhost.to/images/853/445466402_bs.png"),
+        new Block("Scrapy", "https://scrapy.org", "https://img93.pixhost.to/images/853/445466397_31r4now.png"),
+        new Block("playwright-python", "https://playwright.dev/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/playwright/playwright-original.svg"),
     ],
     "IDE": [
         new Block("Pycharm", "https://www.jetbrains.com/pycharm/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pycharm/pycharm-original.svg"),
@@ -133,9 +141,6 @@ const SKILL = {
         new Block("Webstorm", "https://www.jetbrains.com/webstorm/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webstorm/webstorm-original.svg"),
         new Block("vscode", "https://code.visualstudio.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"),
         new Block("VisualStudio", "https://visualstudio.microsoft.com/zh-hant/downloads/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg"),
-        new Block("Phpstorm", "https://www.jetbrains.com/phpstorm/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/phpstorm/phpstorm-original.svg"),
-
-
     ],
     "Other": [
         new Block("Anaconda", "https://www.anaconda.com/products/individual", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/anaconda/anaconda-original-wordmark.svg"),
@@ -143,10 +148,8 @@ const SKILL = {
         new Block("git", "https://git-scm.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg"),
         new Block("github", "https://github.com/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"),
         new Block("putty", "https://www.putty.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/putty/putty-original.svg"),
-        new Block("sqlalchemy", "https://www.sqlalchemy.org/", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original-wordmark.svg"),
         new Block("SSH", "https://zh.wikipedia.org/zh-tw/Secure_Shell", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ssh/ssh-original-wordmark.svg"),
-
-
+        new Block("Swagger", "https://swagger.io/", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original-wordmark.svg"),
     ]
 
 }
@@ -154,28 +157,28 @@ const SKILL = {
 const WORKED_PROJECT = [
     new Card(
         "TW Ranking",
-        imagesFromGoogleDrive("1bUa6mWkSSuX7cNljirTFdUndCwa98sHG"),
+        "https://img93.pixhost.to/images/853/445466418_omyriog.png",
         "TW Ranking由國立臺灣大學圖書資訊學系 黃慕萱教授主持。始於2018年，與臺灣大學計量理論與應用中心合作發表，期望透過客觀量化分析的方式，針對臺灣之大專院校，設計一套多數學校現階段經努力即可達成的適切指標。",
         "http://twranking.lis.ntu.edu.tw/about/",
         false
     ),
     new Card(
         "NTU Ranking",
-        imagesFromGoogleDrive("1TQ-XUpHYak8xU2NwTimQjsJQDHxO6n7U"),
+        "https://img93.pixhost.to/images/853/445466438_ruksw3t.png",
         "The \"Performance Ranking of Scientific Papers for World Universities\" is released by National Taiwan University, and is also known as NTU Ranking. NTU Ranking provides overall ranking, rankings by six fields, and rankings by 24 selected subjects.",
         "http://nturanking.csti.tw/",
         true
     ),
     new Card(
         "NTU Innovation Ranking",
-        imagesFromGoogleDrive("1GQKEuFniXRVCjCBSDzxAtDoiTtcZxcb3"),
+        "https://img93.pixhost.to/images/853/445466450_yhyww6k.png",
         "由國立臺灣大學科研計量研究中心(Center for Science and Technology Informetrics, CSTI)陳達仁教授主持，透過易讀的指標、資料視覺化，讓閱覽者能快速、全面地了解各國與企業的專利創新表現。本研究蒐集美國專利資料局(USPTO)所提供的公開資料，進行量化的研究分析，透過此研究呈現各國與企業在專利領域的創新表現，以及主導專利表現的國家及企業，提供各界較為客觀的參考依據。",
         "http://140.112.180.163:8002/",
         true
     ),
     new Card(
         "科研計量研究中心",
-        imagesFromGoogleDrive("1gJyECa6MKbP1PdS7joKtLPTjaURTd8Dh"),
+        "https://img93.pixhost.to/images/853/445466399_38ulz7o.png",
         `<ul>
                     <li>協助國立臺灣大學科研計量研究中心，整合中心相關資源與事蹟另如研究計畫、學術活動、研究成果。</li>
                     <li>使用Python與Django MVC 搭配前端語言進行設計與建置。</li>
@@ -185,7 +188,7 @@ const WORKED_PROJECT = [
     ),
     new Card(
         "T醫院-精神居家醫療照護系統",
-        imagesFromGoogleDrive("1dm8-sRqiQau7ItfofsqYIvN31BVABBZi"),
+        "https://img93.pixhost.to/images/854/445468998_snipaste_2024-02-12_18-06-10.jpg",
         `<ul>
                     <li>協助彙整聯合醫院精神科訪視紙本填寫表單，整合為網頁化、電子化管理。</li>
                     <li>使用PHP、MySql、與前端網站語言進行功能開發，包含輸入、表格、拖曳式個案資料配對等功能。</li>
@@ -196,7 +199,7 @@ const WORKED_PROJECT = [
     ),
     new Card(
         "K工廠 CPS System",
-        imagesFromGoogleDrive("1WiMM7ouPULa2FqAez6USh4HIJ6uObAFF"),
+        "https://img93.pixhost.to/images/854/445468999_snipaste_2024-02-12_18-07-08.jpg",
         `<ul>
                     <li>協助該工廠廠區與該企業資訊部整合機台與工單資料，並梳理資料流(包含資料取得、資料儲存與資料統計顯示)，並提供單一整合型資訊看板，供企業決策層與企業使用者進行查看。</li>
                     <li>使用PHP、MySql、與前端網站語言進行功能開發，整合視覺化套件與表格套件提供互動式查詢。</li>
@@ -207,7 +210,7 @@ const WORKED_PROJECT = [
     ),
     new Card(
         "S傳統產業ERP整合系統",
-        imagesFromGoogleDrive("1LLb-lap93dvMr5FKdL3H5l7tbuvTq7xt"),
+        "https://img93.pixhost.to/images/853/445466449_vke4xlp.png",
         `<ul>
                     <li>協助該傳統展業建立ERP管理系統，並配合既有的API、資料庫來建置如管理方帳號管理、派單作業、庫存管理、歷史統計等功能；客戶端提供子帳號管理、訂單歷程與銷帳查詢。</li>
                     <li>使用C# MVC建立網站後端，並搭配前端語言進行實作。</li>
@@ -218,7 +221,7 @@ const WORKED_PROJECT = [
     ),
     new Card(
         "H機構 住宅資訊系統",
-        imagesFromGoogleDrive("105iU1M_tVvSuMECWY7ZbIx7ERTSeZwrT"),
+        "https://img93.pixhost.to/images/853/445466408_fjtshd4.png",
         `<ul>
                     <li>依照H機構建置需求，以前後分離方式且以微服務提供API，支援前後台端功能實作，包含住宅基本資料、基地現況、未來建物模擬圖、基地現場CCTV、會議文件等管理功能。</li>
                     <li>使用django rest framework 作為API框架。</li>
@@ -229,7 +232,7 @@ const WORKED_PROJECT = [
     ),
     new Card(
         "H機構 都更資訊系統",
-        imagesFromGoogleDrive("1ChnLJA3iOM6gfushH5dY5__T6b0ght6L"),
+        "https://img93.pixhost.to/images/853/445466411_hd4e50v.png",
         `<ul>
                     <li>依照H機構建置需求，以前後分離方式且以微服務提供API，支援前後台端功能實作，包含最新進度、進度流程、基礎資料、內容規劃、施工監控、圖片文件等管理功能。</li>
                     <li>使用django rest framework 作為API框架。</li>
@@ -254,7 +257,7 @@ const WORKED_PROJECT = [
 const SIDE_PROJECT = [
     new Card(
         "到處看展覽",
-        imagesFromGoogleDrive("1B0TI4wVRXUNuVkvMR4_EKDvOSH053JSF"),
+        "https://img93.pixhost.to/images/853/445466445_snipaste_2022-10-07_19-13-44.png",
         `<ul>
                     <li>使用Python 爬蟲針對數個 文創園區/博物館/售票網 網站來爬取相關展覽</li>
                     <li>資料更新透過 github action 定時排程更新</li>
@@ -268,7 +271,7 @@ const SIDE_PROJECT = [
     ),
     new Card(
         "Let Me Have a Look This Company (讓我看看！ 這家公司！)",
-        imagesFromGoogleDrive("1fk6nWQzDg09MNmewkSnJ69_e4fXSZjb2"),
+        "https://img93.pixhost.to/images/853/445466443_snipaste_2022-10-07_19-12-55.png",
         `<ul>
                     <li>使用Python FastAPI，針對輸入的公司名稱進行爬蟲</li>
                     <li>爬蟲標的包含
